9c7a67207fcce6df1748d4176f4fa9919298807f
sunilmsd007 <sunilmsd007@gmail.com>
2022-09-07 17:06:02 +0530

    Added: roll dice to get number between 1-6

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -4,6 +4,8 @@ public class SnakeAndLadderGame {
 
 	static int position=0;
         public static void main(String[] args) {
-
+           
+           int diceNumber=(int) Math.floor(Math.random()*10)%6 + 1;
+           System.out.println("Dice Number is: " +diceNumber);
   }
 }
