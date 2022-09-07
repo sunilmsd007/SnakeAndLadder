aa3eeba4fa5ead3d676d70233e13bfc245b0f4eb
sunilmsd007 <sunilmsd007@gmail.com>
2022-09-07 17:18:02 +0530

    player check for movement to make

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -3,9 +3,23 @@ package snakeAndLadder;
 public class SnakeAndLadderGame {
 
 	static int position=0;
+        public static final int LADDER=1;
+	public static final int SNAKE=2;
         public static void main(String[] args) {
            
            int diceNumber=(int) Math.floor(Math.random()*10)%6 + 1;
            System.out.println("Dice Number is: " +diceNumber);
+           int checkMovement= (int) Math.floor(Math.random()*10)%3;
+		switch(checkMovement) {
+		case LADDER:
+			position+=diceNumber;
+			break;
+		case SNAKE:
+			position-=diceNumber;
+			break;
+		default:
+			System.out.println("stay in same position");
+		}
+                System.out.println("Current position is: " +position);
   }
 }
